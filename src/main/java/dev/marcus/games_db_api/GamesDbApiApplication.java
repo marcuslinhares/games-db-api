61e01deb10fe12db06b439c6c1c3efbd9866d241
Marcus Linhares <marcusviniciuslinhares543@gmail.com>
2025-05-13 11:20:21 -0300

    feat: adiciona estrutura inicial do projeto
    - adiciona dockerfile para build em plataformas de deploy
    - adiciona dockercompose para banco de desenvolvimento local
    - adiciona .gitignore
    - adiciona dependencias iniciais no pom.xml
    - adiciona configuração inicial do application.properties

---
+++ src/main/java/dev/marcus/games_db_api/GamesDbApiApplication.java
@@ -0,0 +1,13 @@
+package dev.marcus.games_db_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesDbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesDbApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/marcus/games_db_api/GamesDbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marcus.games_db_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesDbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
