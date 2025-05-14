a6d99ec21df0034a22ca4e1c7430b30b7e8182ad
Marcus Linhares <marcusviniciuslinhares543@gmail.com>
2025-05-14 14:59:10 -0300

    feat: adiciona metodo com retorno paginado de conslta no repository

--- src/main/java/dev/marcus/games_db_api/repositories/DesenvolvedorRepository.java
+++ src/main/java/dev/marcus/games_db_api/repositories/DesenvolvedorRepository.java
@@ -1,9 +1,11 @@
 package dev.marcus.games_db_api.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dev.marcus.games_db_api.domain.entities.DesenvolvedorEntity;
 
 public interface DesenvolvedorRepository extends JpaRepository<DesenvolvedorEntity, Long>{
-
+    Page<DesenvolvedorEntity> findAll(Pageable pageable);
 }
