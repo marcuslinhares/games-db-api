c7e7a5d5030ce8becb4890ac96950d6dca6d5374
Marcus Linhares <marcusviniciuslinhares543@gmail.com>
2025-05-14 21:27:58 -0300

    feat: adiciona endpoints

--- src/main/java/dev/marcus/games_db_api/controllers/JogoController.java
+++ src/main/java/dev/marcus/games_db_api/controllers/JogoController.java
@@ -1,5 +1,6 @@
 package dev.marcus.games_db_api.controllers;
 
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -24,4 +25,51 @@ public interface JogoController {
         ReqRegistroJogoDTO dto,
         UriComponentsBuilder uriBuilder
     );
+
+    @Operation(
+        summary = "Busca todos os jogos do sistema.",
+        responses = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", ref = "badRequest"),
+            @ApiResponse(responseCode = "500", ref = "internalServerError")
+        }
+    )
+    ResponseEntity<Page<ResRegistroJogoDTO>> findAll(
+        int page, int size
+    );
+
+    @Operation(
+        summary = "Busca todos os jogos de um desenvolvedor do sistema.",
+        responses = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", ref = "badRequest"),
+            @ApiResponse(responseCode = "500", ref = "internalServerError")
+        }
+    )
+    ResponseEntity<Page<ResRegistroJogoDTO>> findByDesenvolvedor(
+        Long id, int page, int size
+    );
+
+    @Operation(
+        summary = "Busca todos os jogos de um console do sistema.",
+        responses = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", ref = "badRequest"),
+            @ApiResponse(responseCode = "500", ref = "internalServerError")
+        }
+    )
+    ResponseEntity<Page<ResRegistroJogoDTO>> findByConsole(
+        Long id, int page, int size
+    );
+
+    @Operation(
+        summary = "Busca um jogo pelo id.",
+        responses = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", ref = "badRequest"),
+            @ApiResponse(responseCode = "404"),
+            @ApiResponse(responseCode = "500", ref = "internalServerError")
+        }
+    )
+    ResponseEntity<ResRegistroJogoDTO> findById(Long id);
 }
--- src/main/java/dev/marcus/games_db_api/controllers/impl/JogoControllerImpl.java
+++ src/main/java/dev/marcus/games_db_api/controllers/impl/JogoControllerImpl.java
@@ -1,9 +1,15 @@
 package dev.marcus.games_db_api.controllers.impl;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -29,6 +35,50 @@ public class JogoControllerImpl implements JogoController{
         var response = jogoService.save(dto);
         var uri = uriBuilder.path("/jogo/{id}").buildAndExpand(response.codigo()).toUri();
         return ResponseEntity.created(uri).body(response);
+    }
+
+    @Override
+    @GetMapping
+    public ResponseEntity<Page<ResRegistroJogoDTO>> findAll(
+        @RequestParam int page, @RequestParam int size
+    ) {
+        return ResponseEntity.status(HttpStatus.OK).body(
+            this.jogoService.findAll(PageRequest.of(page, size))
+        );
+    }
+
+    @Override
+    @GetMapping(path = "/desenvolvedor/{id}")
+    public ResponseEntity<Page<ResRegistroJogoDTO>> findByDesenvolvedor(
+        @PathVariable Long id, @RequestParam int page, @RequestParam int size
+    ) {
+        return ResponseEntity.status(HttpStatus.OK).body(
+            this.jogoService.findByDesenvolvedor(
+                PageRequest.of(page, size), id
+            )
+        );
+    }
+
+    @Override
+    @GetMapping(path = "/console/{id}")
+    public ResponseEntity<Page<ResRegistroJogoDTO>> findByConsole(
+        @PathVariable Long id, @RequestParam int page, @RequestParam int size
+    ) {
+        return ResponseEntity.status(HttpStatus.OK).body(
+            this.jogoService.findByConsole(
+                PageRequest.of(page, size), id
+            )
+        );
+    }
+
+    @Override
+    @GetMapping("/{id}")
+    public ResponseEntity<ResRegistroJogoDTO> findById(
+        @PathVariable Long id
+    ) {
+        return ResponseEntity.status(HttpStatus.OK).body(
+            this.jogoService.findById(id)
+        );
     }
 
 }
