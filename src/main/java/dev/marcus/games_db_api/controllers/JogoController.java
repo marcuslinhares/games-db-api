9dac196e5df3af1d9cc9bce614771499a84fce5d
Marcus Linhares <marcusviniciuslinhares543@gmail.com>
2025-05-15 13:13:08 -0300

    fix: corrige descrição da rota openapi

--- src/main/java/dev/marcus/games_db_api/controllers/JogoController.java
+++ src/main/java/dev/marcus/games_db_api/controllers/JogoController.java
@@ -10,7 +10,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-@Tag(name = "Desenvolvedores")
+@Tag(name = "Jogos")
 public interface JogoController {
 
     @Operation(
